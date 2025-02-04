f49e73e97566b69dd78418e42522e4d12ee447ee
ssuminski3 <ssuminski3@gmail.com>
2025-02-04 12:49:22 +0100

    Dodaj pliki projektów.

---
+++ mojgit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mojgit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
